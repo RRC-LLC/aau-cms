4fffbb3d67512304d4f5dcd247615c38d05aca29
Sam Lowe <samdassi7@me.com>
2023-01-18 13:52:50 -0500

    Ready for DO

---
+++ config/env/production/database.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      username: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      schema: env("DATABASE_SCHEMA", "public"),
+    },
+  }
+});
