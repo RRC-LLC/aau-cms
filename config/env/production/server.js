284aabc1b4b2b40770816720e9490bf804aea469
Sam Lowe <samdassi7@me.com>
2023-03-28 12:23:43 -0400

    fixed url

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  // url: 'https://api.athensareaurology.com',
+  url: "cms.athensareaurology.com",
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
