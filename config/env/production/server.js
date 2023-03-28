0d4e72c1584d408f1b8d8e498e9889280a247fcc
Sam Lowe <samdassi7@me.com>
2023-03-28 12:35:48 -0400

    url fix

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  url: "cms.athensareaurology.com",
+  url: "https://cms.athensareaurology.com",
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
